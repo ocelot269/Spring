1bd2148f04b8e36d28ba0e38988f2f5f690d6d05
unknown <ocelot269@gmail.com>
2019-04-25 23:49:31 +0200

    pasado el test numero 6

--- setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
+++ setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.sound.midi.MidiDevice.Info;
+import javax.websocket.server.PathParam;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -45,6 +47,21 @@ public class PersonalController {
 	@RequestMapping(path="/persona/{id}")// {id} esto indica el parametro que le pasas por url
 	@ResponseBody
 	public String persona(@PathVariable("id") int id) { // Aqui le dices que use el del path
-		return getBaseDeDades().get(id);
+		return getBaseDeDades().get(id);	
 	}
+	
+	
+	@RequestMapping(path ="/afegir", method = RequestMethod.POST)	
+	@ResponseBody
+	public String afegir(@RequestParam(value="nom",required=true) String nom) {
+		if (nom!=null) {
+			getBaseDeDades().add(nom);
+			return "ok";
+		}
+		else {
+			return "No ok";
+		}
+	}
+		
+	
 }
