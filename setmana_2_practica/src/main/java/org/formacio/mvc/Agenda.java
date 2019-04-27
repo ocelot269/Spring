2b4cdd49d2e3ff0443ce4ed07261818ac3c5f96e
unknown <ocelot269@gmail.com>
2019-04-27 16:54:24 +0200

    todo los test pasados

--- setmana_2_practica/src/main/java/org/formacio/mvc/Agenda.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/Agenda.java
@@ -5,13 +5,18 @@ import javax.xml.bind.annotation.XmlRootElement;
 import org.formacio.repositori.AgendaService;
 import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @Controller
+
 public class Agenda {
 	
 	//Variables
@@ -53,4 +58,22 @@ public class Agenda {
 		}
 		
 	}
+	
+	
+	
+	@ResponseStatus(code=HttpStatus.NOT_FOUND, reason="Pagina no encontrada")
+	@ExceptionHandler()
+	public String error(Exception noExiseContacto) {
+		return "Hi ha hagut un error a la operació: " + noExiseContacto.getMessage();
+	}
+	
+	
+	@RequestMapping(path="/afegir" , method = RequestMethod.POST)
+	@ResponseBody
+	public void agregarPersona(String id, String nom, String telefon) {
+		
+		getAgenda().inserta(id, nom, telefon);
+	}
+	
+	
 }
