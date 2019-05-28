4828d19e91ccd7cd51ad65cfcfa5a7a991cd43cd
unknown <ocelot269@gmail.com>
2019-05-28 20:55:22 +0200

    pasado el test afegir producte

--- 12_setmana_3_exercici/src/main/java/org/formacio/servei/FacturesService.java
+++ 12_setmana_3_exercici/src/main/java/org/formacio/servei/FacturesService.java
@@ -1,9 +1,21 @@
 package org.formacio.servei;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.formacio.domain.Factura;
+import org.formacio.domain.LiniaFactura;
+import org.formacio.repositori.FacturesRepositori;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class FacturesService {
-
+	
+	@Autowired
+	FacturesRepositori repositorio;
 	
 	/*
 	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
@@ -14,7 +26,14 @@ public class FacturesService {
 	 * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
 	 */
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
-		
-		return null;
+		LiniaFactura linea = new LiniaFactura();
+		Optional<Factura> factura = repositorio.findById(idFactura);
+		if (factura.isPresent()) {
+			linea.setProducte(producte);
+			linea.setTotal(totalProducte);
+			factura.get().getLinies().add(linea);
+			repositorio.save(factura.get());
+		}
+		return factura.get();
 	}
 }
